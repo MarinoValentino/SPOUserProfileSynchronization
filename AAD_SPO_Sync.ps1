2602504d6989e008c78e9b799285636ebfaba68a
MV <MarinoValentino@users.noreply.github.com>
2019-06-27 20:33:47 +0200

    Update AAD_SPO_Sync.ps1

--- AAD_SPO_Sync.ps1
+++ AAD_SPO_Sync.ps1
@@ -1,61 +1,30 @@
-$TenantName='<Tenant>'
-$JobUser='AzureTask@<Tenant>.onmicrosoft.com' # Need SPO Admin
-$cred = Get-Credential -Message $TenantName -UserName $JobUser # Use Get-AutomationPSCredential for Azure Runbook
+$TenantSite="https://<tenant>.sharepoint.com"
+$JobUser="AzureTask@<tenant>.onmicrosoft.com" # Need SPO Administrator
+$cred = Get-AutomationPSCredential -Name 'AzureTask@mvlab.onmicrosoft.com'
+Connect-PnPOnline $TenantSite -Credentials $cred
 Connect-AzureAD -Credential $cred
-$TenantAdminSite="https://$TenantName-admin.sharepoint.com"
-Connect-PnPOnline $TenantAdminSite -Credentials $cred
-Connect-SPOService -Url $TenantAdminSite -Credential $cred
-$Members = Get-SPOUser -Site "https://$TenantName.sharepoint.com" | Where-Object {$_.UserType -eq 'Member'} | select -Property LoginName
-Write-Host "Total SPO Members:"$Members.Count -ForegroundColor Yellow
-foreach($Member in $Members)
+$users=Get-AzureADUser | ? {$_.ObjectType -eq 'User' -and $_.DirSyncEnabled}
+Write-Output "Total Users to Check:" $users.Count
+Foreach ($user in $users)
 {
-  Write-Host "User SPO Check:"$Member.LoginName -ForegroundColor Yellow
-  Try
-  {
-    $User=Get-SPOUser -LoginName $Member.LoginName -Site "https://$TenantName.sharepoint.com"
-    $UserProfile=Get-PnPUserProfileProperty -Account $Member.LoginName
-    $SPOProperty=$UserProfile.UserProfileProperties.Company
-  }
-  Catch
-  {
-    # Write-Host "Error on SPO user :"$Member.LoginName -ForegroundColor Red
-  }
-  Write "SPO Company:$SPOProperty"
-
-  # Write-Host "User AAD Check:"$Member.LoginName -ForegroundColor Yellow
-  Try
-  {
-    $ADUser=Get-AzureADUser -ObjectId $Member.LoginName
-    $ADProperty=$ADUser.CompanyName
-  }
-  Catch
-  {
-    # Write-Host "Error on AAD user :"$Member.LoginName -ForegroundColor Red
-  }
-  Write "AAD Company:$ADProperty"
-
-  If ($ADProperty -eq $null)
-  {
-    if($SPOProperty)
+    Write "USERPRINCIPALNAME:"$user.UserPrincipalName
+    Write "AAD Company:"$user.CompanyName
+    $UserProfile=Get-PnPUserProfileProperty -Account $user.UserPrincipalName
+    Write "SPO Company:"$UserProfile.UserProfileProperties.Company
+    If ($user.CompanyName -eq $null)
     {
-        Write-Host "Delete SPO Company" -ForegroundColor Green
-        Set-PnPUserProfileProperty -Account $Member.LoginName -Property 'Company' -Value ""
+        If ($UserProfile.UserProfileProperties.Company)
+        {
+            Write-Warning "Remove SPO Company"
+            Set-PnPUserProfileProperty -Account $user.UserPrincipalName -Property 'Company' -Value ""
+        }
     }
     else
     {
-        Write-Host "No AD Company" -ForegroundColor Green
+        If ($user.CompanyName -ne $UserProfile.UserProfileProperties.Company)
+        {
+            Write-Warning "Update SPO Company"
+            Set-PnPUserProfileProperty -Account $user.UserPrincipalName -Property 'Company' -Value $user.CompanyName
+        }
     }
-  }
-  else
-  {
-    if($ADProperty -eq $SPOProperty)
-    {
-        Write-Host "Update not needed" -ForegroundColor Green
-    }
-    else
-    {
-        Write-Host "Update SPO Company to:$ADProperty" -ForegroundColor Green
-        Set-PnPUserProfileProperty -Account $Member.LoginName -Property 'Company' -Value $ADProperty
-    }
-  }
-} 
+}
