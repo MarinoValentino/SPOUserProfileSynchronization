11b4f2243edeb8534fed397b98d695e97e8f8255
MV <MarinoValentino@users.noreply.github.com>
2019-07-05 15:07:49 +0200

    Update AAD_SPO_Sync.ps1

--- AAD_SPO_Sync.ps1
+++ AAD_SPO_Sync.ps1
@@ -1,30 +1,35 @@
-$TenantSite="https://<tenant>.sharepoint.com"
-$JobUser="AzureTask@<tenant>.onmicrosoft.com" # Need SPO Administrator
-$cred = Get-AutomationPSCredential -Name 'AzureTask@mvlab.onmicrosoft.com'
+$TenantSite="https://<TenantName>.sharepoint.com"
+$cred = Get-AutomationPSCredential -Name '<CredentialName>' # Need SPO Admin
 Connect-PnPOnline $TenantSite -Credentials $cred
 Connect-AzureAD -Credential $cred
 $users=Get-AzureADUser | ? {$_.ObjectType -eq 'User' -and $_.DirSyncEnabled}
-Write-Output "Total Users to Check:" $users.Count
+$usercount = $users.Count
+Write-Output "TOTAL USER TO CHECK:$usercount"
 Foreach ($user in $users)
 {
-    Write "USERPRINCIPALNAME:"$user.UserPrincipalName
-    Write "AAD Company:"$user.CompanyName
+    $UserPrincipalName = $user.UserPrincipalName
+    Write-Output "CHECK USERPRINCIPALNAME:$UserPrincipalName"
     $UserProfile=Get-PnPUserProfileProperty -Account $user.UserPrincipalName
-    Write "SPO Company:"$UserProfile.UserProfileProperties.Company
+    $AAD_Property = $user.CompanyName
+    $SPO_Property = $UserProfile.UserProfileProperties.Company
+    Write-Output "AAD_PROPERTY:$AAD_Property - SPO_PROPERTY Property:$SPO_Property"
     If ($user.CompanyName -eq $null)
     {
         If ($UserProfile.UserProfileProperties.Company)
         {
-            Write-Warning "Remove SPO Company"
+            Write-Warning "Update User: $UserPrincipalName, remove SPO Company"
             Set-PnPUserProfileProperty -Account $user.UserPrincipalName -Property 'Company' -Value ""
+            Write-Output "Removed."
         }
     }
     else
     {
         If ($user.CompanyName -ne $UserProfile.UserProfileProperties.Company)
         {
-            Write-Warning "Update SPO Company"
+            Write-Warning "Update User: $UserPrincipalName, SPO Property Company to: $AAD_Property"
             Set-PnPUserProfileProperty -Account $user.UserPrincipalName -Property 'Company' -Value $user.CompanyName
+            Write-Output "Updated."
         }
     }
+    Write "--------------------------------"
 }
